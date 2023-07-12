be9b83290a47e4348a5a1256873fcbd65ec03066
Camila Wolter <camila0wolter@gmail.com>
2023-07-11 23:10:24 -0300

    feat: creation of the Influencer migrations

---
+++ app/backend/migrations/20230712004030-create-influencer.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('influencers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      userName: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      subscribers: {
+        type: Sequelize.INTEGER
+      },
+      platform: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('influencers');
+  }
+};
