e816371392632ce0b887da4f486aabae9b7d5ef4
Camila Wolter <camila0wolter@gmail.com>
2023-07-11 23:08:09 -0300

    feat: connection with sequelize

--- app/backend/index.js
+++ app/backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const sequelize = require('./config/path');
 
 
 const app = express();
@@ -12,6 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('API is running');
 });
+
+
+sequelize.sync()
+  .then(() => {
+    console.log('Database and tables created');
+  })
+  .catch((error) => {
+    console.error('Error creating database and tables:', error);
+  });
+
 
   
 const port = 3000;
