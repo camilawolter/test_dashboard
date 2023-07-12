2a3eb5dcb87fa608a953bcaa33b75098b6c39e16
Camila Wolter <camila0wolter@gmail.com>
2023-07-11 23:07:04 -0300

    feat: configuring the environment

---
+++ app/backend/config/config.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: '123456',
+    database: 'influencers_data',
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: 'mysql'
+  }
+};
